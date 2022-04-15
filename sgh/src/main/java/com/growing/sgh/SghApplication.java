83ff4d29808ad40f2ea57952a79a37b9677e7c76
SimsMan <sgh1939@daum.net>
2022-04-15 15:43:26 +0900

    초기 설정

---
+++ sgh/src/main/java/com/growing/sgh/SghApplication.java
@@ -0,0 +1,13 @@
+package com.growing.sgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SghApplication.class, args);
+	}
+
+}
