2390fa4ed8d0fdb011ef01879538741f24692384
SimsMan <sgh1939@daum.net>
2022-04-30 19:54:06 +0900

    feat(cart): 상품을 장바구니에 추가 기능 구현

--- sgh/src/main/java/com/growing/sgh/domain/cart/repository/CartItemRepository.java
+++ sgh/src/main/java/com/growing/sgh/domain/cart/repository/CartItemRepository.java
@@ -4,4 +4,6 @@ import com.growing.sgh.domain.cart.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 }
--- sgh/src/main/java/com/growing/sgh/domain/cart/service/CartService.java
+++ sgh/src/main/java/com/growing/sgh/domain/cart/service/CartService.java
@@ -1,12 +1,21 @@
 package com.growing.sgh.domain.cart.service;
 
+import com.growing.sgh.domain.cart.dto.CartItemDto;
+import com.growing.sgh.domain.cart.entity.Cart;
+import com.growing.sgh.domain.cart.entity.CartItem;
 import com.growing.sgh.domain.cart.repository.CartItemRepository;
 import com.growing.sgh.domain.cart.repository.CartRepository;
+import com.growing.sgh.domain.item.entity.Item;
 import com.growing.sgh.domain.item.repository.ItemRepository;
+import com.growing.sgh.domain.member.entity.Member;
 import com.growing.sgh.domain.member.repository.MemberRepository;
+import com.growing.sgh.exception.item.ItemImgNotFoundException;
+import com.growing.sgh.exception.member.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
@@ -18,7 +27,30 @@ public class CartService {
     private final CartItemRepository cartItemRepository;
 
 
+    public Long addCart(CartItemDto cartItemDto, Long memberId) {
+        Item item = itemRepository.findById(cartItemDto.getItemId()).orElseThrow(ItemImgNotFoundException::new);
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Cart cart = cartRepository.findByMemberId(memberId);
 
+        // 첫 장바구니 사용시, 장바구니 생성
+        checkMemberCart(cart, member);
 
+        CartItem saveCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
 
+        if(Objects.isNull(saveCartItem)){
+            saveCartItem.addCount(cartItemDto.getCount());
+            return saveCartItem.getId();
+        }else{
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+            cartItemRepository.save(cartItem);
+            return cartItem.getId();
+        }
+    }
+
+    private void checkMemberCart(Cart cart, Member member) {
+        if(Objects.isNull(cart)){
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+    }
 }
